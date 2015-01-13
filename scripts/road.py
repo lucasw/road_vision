6b81b8cd6791bd9b965d8d9fdbd4d31c61a8715b
lucasw <lucasw_backwards@gmail.com>
2015-01-13 04:57:34 -0800

    Can paint on image, but need overlay to be overlaid on loaded image

--- scripts/road.py
+++ scripts/road.py
@@ -4,13 +4,87 @@
 # the road, lane dividers, other cars, the sky, and unknown other stuff.
 
 import cv2
+import numpy as np
 import sys
+
+
+def get_mouse(event, x, y, flags, param):
+    param.mx = x
+    param.my = y
+    do_print = True
+    if event == cv2.EVENT_MOUSEMOVE:
+        do_print = False
+    elif event == cv2.EVENT_LBUTTONDOWN:
+        if not param.l_button_down:
+            print 'left button down'
+        param.l_button_down = True
+    elif event == cv2.EVENT_LBUTTONUP:
+        if param.l_button_down:
+            print 'left button up'
+        param.l_button_down = False
+    
+    if do_print:
+        print event, x, y
+
 
 name = sys.argv[1]
 
-im = cv2.imread(name)
+class RoadVision():
+    
+    def __init__(self, name):
+        self.ox = None
+        self.oy = None
+        self.mx = None
+        self.my = None
+        self.l_button_down = False
 
-cv2.imshow("image", im)
-cv2.waitKey(0)
+        self.im = cv2.imread(name)
+        self.overlay = np.zeros(self.im.shape, np.uint8) 
 
+        cv2.namedWindow("image")
+        cv2.setMouseCallback('image', get_mouse, self)
 
+    def spin(self):
+        while True:
+            if self.l_button_down:
+                col = (255,0,0)
+                sz = 20
+                if (self.ox is not None and self.mx is not None):
+                    cv2.line(self.overlay, (self.ox, self.oy),
+                            (self.mx, self.my), col, sz)
+                self.ox = self.mx
+                self.oy = self.my
+                #print self.mx, self.my
+                #cv2.circle(self.overlay, (self.mx, self.my), sz, col, -1) 
+            cv2.imshow("image", self.overlay)
+            key = cv2.waitKey(5)
+            #if key != -1:
+            #    print key
+            if key == ord('q'):
+                break
+
+comment = '''
+>>> import cv2
+>>> events = [i for i in dir(cv2) if 'EVENT' in i]
+>>> print events
+['EVENT_FLAG_ALTKEY', 
+'EVENT_FLAG_CTRLKEY', 
+'EVENT_FLAG_LBUTTON', 
+'EVENT_FLAG_MBUTTON', 
+'EVENT_FLAG_RBUTTON', 
+'EVENT_FLAG_SHIFTKEY',
+'EVENT_LBUTTONDBLCLK', 
+'EVENT_LBUTTONDOWN', 
+'EVENT_LBUTTONUP', 
+'EVENT_MBUTTONDBLCLK', 
+'EVENT_MBUTTONDOWN', 
+'EVENT_MBUTTONUP', 
+'EVENT_MOUSEMOVE', 
+'EVENT_RBUTTONDBLCLK', 
+'EVENT_RBUTTONDOWN',
+'EVENT_RBUTTONUP']
+'''
+
+if __name__ == '__main__':
+    road_vision = RoadVision(sys.argv[1])
+    road_vision.spin()
