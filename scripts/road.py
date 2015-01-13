622fb90d7e8599019a7df598c48654a43bf17db0
lucasw <lucasw_backwards@gmail.com>
2015-01-13 04:11:55 -0800

    load image

--- scripts/road.py
+++ scripts/road.py
@@ -2,3 +2,15 @@
 
 # load images of driving down a road, find different features -
 # the road, lane dividers, other cars, the sky, and unknown other stuff.
+
+import cv2
+import sys
+
+name = sys.argv[1]
+
+im = cv2.imread(name)
+
+cv2.imshow("image", im)
+cv2.waitKey(0)
+
+
